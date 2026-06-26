37483e7b6467b252666f64f54678290f5097dd8f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-25 22:22:00 -0700

    Add item2 class NutritionFacts

--- ch2/src/item2/NutritionFacts.java
+++ ch2/src/item2/NutritionFacts.java
@@ -28,4 +28,13 @@ public class NutritionFacts {
         this.fat = fat;
         this.sodium = sodium;
     }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
 }
