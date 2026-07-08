54ef25a828eb29e9d9dddbe9e900995b107b420a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-08 07:15:22 +0330

    add class NutritionFacts

--- ch2/src/item2/NutritionFacts.java
+++ ch2/src/item2/NutritionFacts.java
@@ -1,12 +1,12 @@
 package item2;
 
 public class NutritionFacts {
-    private int servingSize =-1;
-    private int servings =-1;
-    private int calories = 0;
-    private int fat = 0;
-    private int sodium = 0;
-    private int carbohydrate = 0;
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
 
 
 }
